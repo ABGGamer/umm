91855cd7cd1a99e5acb38557ad942d633f5a8337
ABGGamer <49562543+ABGGamer@users.noreply.github.com>
2021-03-23 21:18:29 +0100

    Add files via upload

---
+++ sample/xlmrpc.php
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?><rsd version="1.0" xmlns="http://archipelago.phrasewise.com/rsd">
+	<service>
+		<engineName>NemesisX</engineName>
+		<engineLink></engineLink>
+		<homePageLink></homePageLink>
+		<apis>
+			<api name="WordPress" blogID="1" preferred="true" apiLink="https://user-68311798-work.colibriwp.com/sample-project/xmlrpc.php" />
+			<api name="Movable Type" blogID="1" preferred="false" apiLink="https://user-68311798-work.colibriwp.com/sample-project/xmlrpc.php" />
+			<api name="MetaWeblog" blogID="1" preferred="false" apiLink="https://user-68311798-work.colibriwp.com/sample-project/xmlrpc.php" />
+			<api name="Blogger" blogID="1" preferred="false" apiLink="https://user-68311798-work.colibriwp.com/sample-project/xmlrpc.php" />
+				<api name="WP-API" blogID="1" preferred="false" apiLink="https://user-68311798-work.colibriwp.com/sample-project/wp-json/" />
+			</apis>
+	</service>
+</rsd>
